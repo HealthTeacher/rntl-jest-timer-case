972d88d27455f9cda212e24df9f3e20badb54683
David Calhoun <dpcalhoun@gmail.com>
2020-09-09 13:54:46 -0400

    Replace existing tests with RNTL tests
    Copy paste tests from RNTL repository to reduce variables.

--- modern-timers.test.js
+++ modern-timers.test.js
@@ -1,26 +1,114 @@
-import { waitFor } from "@testing-library/react-native";
+// @flow
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
 
-// Enabling mock timers breaks `waitFor` in the second test below
-// jest.useFakeTimers();
+class Banana extends React.Component {
+  changeFresh = () => {
+    this.props.onChangeFresh();
+  };
 
-// Replacing the above with "modern" timers causes all three tests to fail
-jest.useFakeTimers("modern");
+  render() {
+    return (
+      <View>
+        {this.props.fresh && <Text>Fresh</Text>}
+        <TouchableOpacity onPress={this.changeFresh}>
+          <Text>Change freshness!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
-test("it successfully runs tests", () => {
-  expect(true).toBeTruthy();
-});
+class BananaContainer extends React.Component {
+  state = { fresh: false };
 
-test("it successfully uses waitFor", async () => {
-  await waitFor(() => {
-    expect(true).toBeTruthy();
-  });
-});
+  onChangeFresh = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    this.setState({ fresh: true });
+  };
 
-test("it successfully uses waitFor with real timers", async () => {
-  // Manually enabling real timers makes `waitFor` work, but this isn't
-  // applicable in a test where you'd like to pair `waitFor` with fake timers.
+  render() {
+    return (
+      <Banana onChangeFresh={this.onChangeFresh} fresh={this.state.fresh} />
+    );
+  }
+}
+
+afterEach(() => {
   jest.useRealTimers();
-  await waitFor(() => {
-    expect(true).toBeTruthy();
+});
+
+test("waits for element until it stops throwing", async () => {
+  const { getByText, queryByText } = render(<BananaContainer />);
+
+  fireEvent.press(getByText("Change freshness!"));
+
+  expect(queryByText("Fresh")).toBeNull();
+
+  const freshBananaText = await waitFor(() => getByText("Fresh"));
+
+  expect(freshBananaText.props.children).toBe("Fresh");
+});
+
+test("waits for element until timeout is met", async () => {
+  const { getByText } = render(<BananaContainer />);
+
+  fireEvent.press(getByText("Change freshness!"));
+
+  await expect(
+    waitFor(() => getByText("Fresh"), { timeout: 100 })
+  ).rejects.toThrow();
+
+  // Async action ends after 300ms and we only waited 100ms, so we need to wait
+  // for the remaining async actions to finish
+  await waitFor(() => getByText("Fresh"));
+});
+
+test("waits for element with custom interval", async () => {
+  const mockFn = jest.fn(() => {
+    throw Error("test");
   });
+
+  try {
+    await waitFor(() => mockFn(), { timeout: 400, interval: 200 });
+  } catch (e) {
+    // suppress
+  }
+
+  expect(mockFn).toHaveBeenCalledTimes(3);
+});
+
+test("works with legacy fake timers", async () => {
+  jest.useFakeTimers("legacy");
+
+  const mockFn = jest.fn(() => {
+    throw Error("test");
+  });
+
+  try {
+    waitFor(() => mockFn(), { timeout: 400, interval: 200 });
+  } catch (e) {
+    // suppress
+  }
+  jest.advanceTimersByTime(400);
+
+  expect(mockFn).toHaveBeenCalledTimes(3);
+});
+
+test("works with fake timers", async () => {
+  jest.useFakeTimers("modern");
+
+  const mockFn = jest.fn(() => {
+    throw Error("test");
+  });
+
+  try {
+    waitFor(() => mockFn(), { timeout: 400, interval: 200 });
+  } catch (e) {
+    // suppress
+  }
+  jest.advanceTimersByTime(400);
+
+  expect(mockFn).toHaveBeenCalledTimes(3);
 });
